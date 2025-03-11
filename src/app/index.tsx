ea4e7a6968555d806b1ca33e22495997048b5791
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-11 14:10:50 +0300

    fix bug with expo-splash-screen and install react-native-svg

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,37 +1,52 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import MainTable from 'src/components/mainTable/mainTable';
 import { MatchesContextProvider } from 'src/utils/matchesContext';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
 
 const App = () => {
-  const [font, setFont] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  const fonts = async () => await Font.loadAsync({
-    TacticSansBoldIt: require('../assets/fonts/TacticSansBoldIt.ttf'),
-    TacticSansReg: require('../assets/fonts/TacticSansReg.ttf'),
-    InterMed: require('../assets/fonts/InterMed.ttf'),
-    InterSemiBold: require('../assets/fonts/InterSemiBold.ttf'),
-  });
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          TacticSansBoldIt: require('../assets/fonts/TacticSansBoldIt.ttf'),
+          TacticSansReg: require('../assets/fonts/TacticSansReg.ttf'),
+          InterMed: require('../assets/fonts/InterMed.ttf'),
+          InterSemiBold: require('../assets/fonts/InterSemiBold.ttf'),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
 
-  if(font) {
-    return (
-      <MatchesContextProvider>
-        <View style={styles.body}>
-          <MainTable />
-        </View>
-      </MatchesContextProvider>
-    );
-  } else {
-    return (
-      <AppLoading
-        startAsync={fonts}
-        onError={err => console.log(err)}
-        onFinish={() => setFont(true)}
-      />
-    )
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <MatchesContextProvider>
+         <View style={styles.body}>
+           <MainTable />
+         </View>
+       </MatchesContextProvider>
+    </View>
+  );
 };
 
 export default App;
