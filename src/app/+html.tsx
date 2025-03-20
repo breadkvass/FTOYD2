82999b189a7eefff4468cfcb31cdb8524744bee3
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-20 11:32:43 +0300

    upd

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -8,9 +8,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
-        <title>FTOYD | тестовое 2</title>
-        <meta name='title' content='Expo Router Typescript' />
-        <meta name='description' content='Тестовое задание для FTOYD' />
+        <title></title>
         <meta name='author' content='Sulimova Anastasiia' />
         <ScrollViewStyleReset />
       </head>
