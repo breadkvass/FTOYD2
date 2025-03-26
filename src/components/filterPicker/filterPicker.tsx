9d0797d570e8402555ad3e582718ed85d5953e1b
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-26 20:58:58 +0300

    Revert "first commit"
    This reverts commit ef08984755444b73e2155e678c9ac0b53d4f9551.

---
+++ src/components/filterPicker/filterPicker.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet } from 'react-native';
+import { FC, useState } from 'react';
+import { renderPickerButton } from './pickerButton';
+import { renderPickerItem } from './pickerItem';
+import { FilterItem } from 'src/utils/types';
+import SelectDropdown from 'react-native-select-dropdown';
+import { useResize } from 'src/hooks/useResize';
+
+type FilterPickerProps = {
+    setFilterHandler: (selectedItem: any) => any
+}
+
+const FilterPicker: FC<FilterPickerProps> = ({setFilterHandler}) => {
+    const [ isPickerItemHovered, setIsPickerItemHovered ] = useState('');
+    const [ isPickerItemPressed, setIsPickerItemPressed ] = useState('');
+    const [ isPickerHovered, setIsPickerHovered ] = useState(false);
+    
+    const { width, isScreenS } = useResize();
+    
+    const filters: FilterItem[] = [
+        {label: 'Все статусы', value: ''},
+        {label: 'Match preparing', value: 'Scheduled'},
+        {label: 'Live', value: 'Ongoing'},
+        {label: 'Finished', value: 'Finished'},
+    ]
+   
+    return (
+        <SelectDropdown
+            data={filters}
+            onSelect={(selectedItem) => setFilterHandler(selectedItem.value)}
+            renderButton={(selectedItem, isPickerOpen) => renderPickerButton(
+                selectedItem,
+                isPickerOpen,
+                isPickerHovered,
+                setIsPickerHovered,
+                isScreenS,
+                width
+            )}
+            renderItem={(item: FilterItem) => renderPickerItem(
+                item,
+                isPickerItemHovered,
+                isPickerItemPressed,
+                setIsPickerItemHovered,
+                setIsPickerItemPressed
+            )}
+            showsVerticalScrollIndicator={false}
+            dropdownStyle={styles.dropdownMenuStyle}
+        />
+    )
+}
+
+export default FilterPicker;
+
+const styles = StyleSheet.create({
+    dropdownMenuStyle: {
+        backgroundColor: '#0F1318',
+        borderRadius: 8,
+    }
+});
---
+++ src/components/filterPicker/pickerButton.tsx
@@ -0,0 +1,88 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useMemo } from 'react';
+import ArrowDownIcon from '../icons/arrowDownIcon';
+import ArrowUpIcon from '../icons/arrowUpIcon';
+
+export const renderPickerButton = (
+    selectedItem: any,
+    isPickerOpen: boolean,
+    isPickerHovered: boolean,
+    setIsPickerHovered: (isPickerHovered: boolean) => void,
+    isScreen: boolean,
+    width: number
+) => {
+
+    const pickerStyle = useMemo(() => !isScreen ? [styles.picker,  styles.picker_800] : styles.picker, [width]);
+
+    const pickerButtonStyle = (isPickerOpen: boolean) => {
+        return isPickerOpen ? (
+            [ pickerStyle, styles.dropdownButtonOpen ]
+        ) : (
+           isPickerHovered ? (
+                [ pickerStyle, styles.dropdownButtonHover ]
+            ) : (
+                pickerStyle
+            )
+       )
+    }
+
+    const pickerTextStyle = (isPickerOpen: boolean) => {
+        return (!isPickerHovered || !isPickerOpen) ? (
+            styles.dropdownButtonTxtStyle
+        ) : (
+            [ styles.dropdownButtonTxtStyle,  styles.dropdownButtonTextHover ]
+        )
+    }
+
+    return (
+        <View
+            style={pickerButtonStyle(isPickerOpen)}
+            onPointerEnter={() => setIsPickerHovered(true)}
+            onPointerLeave={() => setIsPickerHovered(false)}
+        >
+            <Text style={pickerTextStyle(isPickerOpen)}>
+                {(selectedItem && selectedItem.label) || 'Все статусы'}
+            </Text>
+            {!isPickerOpen ?
+                <ArrowDownIcon color={isPickerHovered ? 'white' : '#B4B5B6'} />
+                :
+                <ArrowUpIcon />
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    picker: {
+        marginRight: 'auto',
+        minWidth: 190,
+        backgroundColor: '#0B0E12',
+        padding: 16,
+        borderWidth: 0,
+        borderRadius: 4,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 56
+    },
+    picker_800: {
+        width: '100%',
+        marginRight: 0
+    },
+    dropdownButtonHover: {
+        backgroundColor: '#0F1318'
+    },
+    dropdownButtonOpen: {
+        backgroundColor: '#0B0E12',
+        borderColor: '#171D25',
+        borderWidth: 1
+    },
+    dropdownButtonTxtStyle: {
+        color: '#B4B5B6',
+        fontFamily: 'InterMed',
+        fontSize: 16,
+    },
+    dropdownButtonTextHover: {
+        color: 'white'
+    }
+});
